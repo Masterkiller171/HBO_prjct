87f7c816f86b1138cb543d6ad98503bb4919bc6c
Youri Bontekoe <youribontekoe@gmail.com>
2018-09-26 19:42:44 +0200

    Plaatjes toegevoegt
    Plaatjes toegevoegt aan de kleine boxjes

--- Course 1/Les 2/JS/Main.js
+++ Course 1/Les 2/JS/Main.js
@@ -1,1 +1,12 @@
-document.body.className = 'fade';
+var img = document.getElementById('avans_log');
+var position = 0;
+var id = setInterval(frame, 10);
+
+function scroller(){ 
+    
+    if(pos == 200){
+        clearInterval(id);
+    }else{
+        img.style.left = position + 'px';
+    }
+}
