c9c0a9c5260ef3723d4213975e2628e4a416a142
Youri Bontekoe <youribontekoe@gmail.com>
2018-10-02 16:06:48 +0200

    txtbar met arrows werkt nu
    *Fix voor txtbar met arrows die vergroten

--- Course 1/Les 2/JS/Main.js
+++ Course 1/Les 2/JS/Main.js
@@ -1,37 +1,28 @@
-//Wanneer de gebruiker op de rechter arrow klikt zal de rechter box 100% worden en dus meer content laten zien
-function clckright() {
-    var leftAr = document.getElementById("left-arr");
-
-    if (leftAr.style.display === "inline-block") {
-
-        document.getElementById("right").style.width = "100%";
-        leftAr.style.display = "none";
-        document.getElementById("right-arr").style.transform = "initial";
-
-    } else {
-
-        document.getElementById("right").style.width = "50%";
-        document.getElementById("left-arr").style.display = "inline-block";
-        document.getElementById("right-arr").style.transform = "initial";
-
-    }
-}
-
 //Wanneeer de gebruiker op de linker arrow klikt verdwijnt het tweede vak zodat dat links 100% word
 function clckleft() {
     var right = document.getElementById("right");
 
     if (right.style.display === "inline-block") {
         right.style.display = "none";
-        document.getElementById("left-arr").setAttribute('style', 'transform: rotate(180deg);');
-        document.getElementById("left-arr").setAttribute('style', 'padding-left: 90%;');
-        document.getElementById("rghtclss").style.width = "80%";
+        document.getElementById("leftarrw").setAttribute('style', 'transform: rotate(180deg);');
     } else {
-
         right.style.display = "inline-block";
-        document.getElementById("left-arr").style.transform = "initial";
-        document.getElementById("left-arr").setAttribute('style', 'padding-left: 0px;');
-        document.getElementById("rghtclss").style.width = "40%";
 
     }
 }
+
+//Wanneer de gebruiker op de rechter arrow klikt zal de rechter box 100% worden en dus meer content laten zien
+function clckright() {
+    var leftAr = document.getElementById("left");
+
+    if (leftAr.style.display === "inline-block") {
+        leftAr.style.display = "none";
+        document.getElementById("rghtarrw").setAttribute('style', 'transform: rotate(180deg);');
+
+    } else {
+        document.getElementById("left").style.display = "inline-block";
+
+    }
+}
+
+
