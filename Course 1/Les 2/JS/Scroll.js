ac3a6bf7bd4c071f9df1b28ebfe0db693556e45d
ThatAwesomeGuy-TAG <youribontekoe@gmail.com>
2018-10-05 12:58:31 +0200

    Add files via upload

--- Course 1/Les 2/JS/Scroll.js
+++ Course 1/Les 2/JS/Scroll.js
@@ -1,13 +1,14 @@
-var lastScroll = 0, hundert;
-$(window).scroll(function(){
-	//NAVIGATION FADEIN/OUT
-	var st = $(document).scrollTop();
-	if (st > 250 && st > lastScroll){
-		$('nav').addClass('hidden');
-		hundert = st;
-	}
-	else if(st < (hundert - 33)){
-		$('nav').removeClass('hidden');
-	}
-  lastScroll = st; 	
-  });
+//Navigatie scroll Fade-in / fade-out
+var lastScroll = 0,
+    hundert;
+$(window).scroll(function () {
+
+    var st = $(document).scrollTop();
+    if (st > 250 && st > lastScroll) {
+        $('nav').addClass('hidden');
+        hundert = st;
+    } else if (st < (hundert - 33)) {
+        $('nav').removeClass('hidden');
+    }
+    lastScroll = st;
+});
